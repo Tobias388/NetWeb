ec35ef3560d190f7e42f6e28f0c6020e6735e2d0
Tobías Oviedo <76019160+Tobias388@users.noreply.github.com>
2024-07-08 16:52:55 -0300

    Delete scripts directory

--- scripts/background_parallax.js
+++
@@ -1,7 +0,0 @@
-const background_img = document.querySelector('.background_img')
-
-window.addEventListener('scroll', () => {
-    let scrollY = document.documentElement.scrollTop
-    background_img.style.transform = 'translateY('+ scrollY * -0.8 +'px)'
-    console.log('scroll')
-})
--- scripts/slider_services.js
+++
@@ -1,8 +0,0 @@
-const slider = document.querySelector('.slider')
-let deg = 0
-
-
-setInterval(() => {
-    deg = deg + 90
-    slider.style.transform = 'rotateZ('+deg+'deg)'
-}, 2500);
